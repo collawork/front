740088f191d7268cf0c20bb0c036b458edf10d1f
kkandda <diobobo@naver.com>
2024-10-31 18:14:18 +0900

    마이 페이지 겸 메인 페이지 작성 중

--- collawork/frontend/src/pages/MyPage.js
+++ collawork/frontend/src/pages/MyPage.js
@@ -13,21 +13,24 @@ import { useNavigate } from "react-router-dom"
 const MyPage =()=>{
     const navigate = useNavigate();
 
+    // 캘린더로 이동
     const moveToCalender = ()=>{
         navigate('/calender');
     };
 
+    // 프로젝트로 이동
     const moveToProject =()=>{
         navigate('/project');
     }
 
+    // 친구 페이지로 이동
     const moveToFirend =()=>{
         navigate('/friend');
     }
     
     return(
         <>
-            
+
         </>
     )   
 }
