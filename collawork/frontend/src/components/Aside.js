524fdc9da04de0e94eca0dc83feafe79a8b55851
seo <cgo2711@naver.com>
2024-11-06 18:10:35 +0900

    asd

--- collawork/frontend/src/components/Aside.js
+++ collawork/frontend/src/components/Aside.js
@@ -1,31 +1,57 @@
 import { useState } from 'react'; 
 import ReactModal from "react-modal"; 
 import ProjectService from "../services/ProjectService"; 
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 const Aside = () => {  
+
+  const [title, setTitle] = useState("");
+  const [context, setContext] = useState("");
    
-    const [formData, setFormData] = useState({ 
-        title: "", 
-        context: "" 
-    });
+    // const [formData, setFormData] = useState({ 
+    //     title: "", 
+    //     context: "" 
+    // });
 
     const [newShow, setNewShow] = useState(false); 
+    function Send(){
+      console.log(title);
+      console.log(context);
+      axios(
+          {
+              url:`${API_URL}/api/user/projects/newproject`,
+              method: 'post',
+              data: {
+                  title:title, context:context
+              },
+              baseURL:'http://localhost:8080',
+              withCredentials: true,
+          }
+      ).then(function(response){
+          console.log(response.data)
+      }
+  )
+  }
 
     
     const modalCloseHandler = () => { 
         setNewShow(false); 
-        setFormData({ title: "", context: "" }); 
+        // setFormData({ title: "", context: "" }); 
+        setTitle("");
+        setContext("");
     };
 
     const handleSubmit = async (e) => { 
         e.preventDefault(); 
 
-        if (!formData.title) {
+        if (!title) {
             alert("프로젝트의 이름을 입력해주세요.");
             return;
         }
         try {
-            await ProjectService.newProjctInsert(formData);
+            Send(title, context);
             alert('새 프로젝트가 생성되었습니다.');
             setNewShow(false);
         } catch (error) {
@@ -34,12 +60,13 @@ const Aside = () => {
     };
 
    
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            [name]: value,
-        }));
+    const handleInputChange1 = (e) => {
+      setTitle(e.target.value);
+       
+    };
+
+    const handleInputChange2 = (e) => {
+      setContext(e.target.value);
     };
 
     return (
@@ -72,15 +99,15 @@ const Aside = () => {
                         type="text" 
                         name="title" 
                         placeholder="제목을 입력하세요" 
-                        onChange={handleInputChange} 
-                        value={formData.title} 
+                        onChange={handleInputChange1} 
+                        value={title} 
                         required 
                     /> 
                     <textarea 
                         name="context" 
                         placeholder='프로젝트 설명을 입력하세요.' 
-                        onChange={handleInputChange} 
-                        value={formData.context} 
+                        onChange={handleInputChange2} 
+                        value={context} 
                     /> 
                     <div> 
                         <button type="submit">프로젝트 생성</button> 
