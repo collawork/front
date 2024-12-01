330db944c8480b665759eef67720282f633b8386
nicdkim <zzsehdrb@naver.com>
2024-12-01 22:43:37 +0900

    userINfo , gear

--- collawork/frontend/src/layout/UserInfoModal.js
+++ collawork/frontend/src/layout/UserInfoModal.js
@@ -7,7 +7,10 @@ const UserInfoModal = ({ user, onClose }) => {
 
     useEffect(() => {
         const handleClickOutside = (event) => {
-            if (modalRef.current && !modalRef.current.contains(event.target)) {
+            if (
+                modalRef.current &&
+                !modalRef.current.contains(event.target)
+            ) {
                 onClose();
             }
         };
@@ -18,18 +21,21 @@ const UserInfoModal = ({ user, onClose }) => {
     }, [onClose]);
 
     return (
-        <div className="user-info-dropdown" ref={modalRef}>
-            <img
-                src={user.profileImageUrl || defaultImage}
-                alt={`${user.username || '사용자'}의 프로필 이미지`}
-                className="profile-image-small"
-            />
-            <p>이름: {user.username || '정보 없음'}</p>
-            <p>이메일: {user.email || '정보 없음'}</p>
-            <p>회사명: {user.company || '정보 없음'}</p>
-            <p>직급: {user.position || '정보 없음'}</p>
-            <p>핸드폰 번호: {user.phone || '정보 없음'}</p>
-            <p>fax: {user.fax || '정보 없음'}</p>
+        <div className="user-info-modal">
+            <div className="modal-content-info" ref={modalRef}>
+                <img
+                    src={user?.profileImageUrl || defaultImage}
+                    alt={`${user?.username || '사용자'}의 프로필 이미지`}
+                    className="profile-image-small"
+                />
+                <p>이름: {user?.username || '정보 없음'}</p>
+                <p>이메일: {user?.email || '정보 없음'}</p>
+                <p>회사명: {user?.company || '정보 없음'}</p>
+                <p>직급: {user?.position || '정보 없음'}</p>
+                <p>핸드폰 번호: {user?.phone || '정보 없음'}</p>
+                <p>fax: {user?.fax || '정보 없음'}</p>
+                <button onClick={onClose}>닫기</button>
+            </div>
         </div>
     );
 };
